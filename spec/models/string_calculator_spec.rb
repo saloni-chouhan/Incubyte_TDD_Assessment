d0ff26a9f39df0dfe9427290bb3f59b427cc4bf0
saloni-chouhan <chouhansaloni99@gmail.com>
2025-06-27 22:36:40 +0530

    RED: Add failing test for custom delimiters

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -26,5 +26,10 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add("1\n2,3")).to eq(6)
     end
+
+    it "supports custom delimiters" do
+      calculator = StringCalculator.new
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
