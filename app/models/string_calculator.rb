dc97f7142a44db4d565084662ee1219fb6c9d504
saloni-chouhan <chouhansaloni99@gmail.com>
2025-06-27 22:27:00 +0530

    REFACTOR: Extract parse_numbers method

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,8 +1,13 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+    
+    parse_numbers(numbers).sum
+  end
 
-    number_array = numbers.split(',')
-    number_array.map(&:to_i).sum
+  private
+
+  def parse_numbers(numbers)
+    numbers.split(',').map(&:to_i)
   end
 end
