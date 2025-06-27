db79c7d50264f0975614dbaaef8c6436175c298b
saloni-chouhan <chouhansaloni99@gmail.com>
2025-06-27 22:33:31 +0530

    RED: Add failing test for newlines between numbers

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -21,5 +21,10 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add("1,2,3,4,5")).to eq(15)
     end
+
+    it "handles newlines between number" do
+      calculator = StringCalculator.new
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
