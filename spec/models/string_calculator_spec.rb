351753c6acd0c28fdf257faaa35ef426fdba70ca
saloni-chouhan <chouhansaloni99@gmail.com>
2025-06-27 22:31:56 +0530

    RED: Add test for multiple numbers (already passes)

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -16,5 +16,10 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add("1,5")).to eq(6)
     end
+
+    it "handles any amount of numbers" do
+      calculator = StringCalculator.new
+      expect(calculator.add("1,2,3,4,5")).to eq(15)
+    end
   end
 end
