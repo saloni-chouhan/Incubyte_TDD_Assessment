2ecf315085344510b0de7d6993734edaed0061b5
saloni-chouhan <chouhansaloni99@gmail.com>
2025-06-28 10:44:13 +0530

    REFACTOR: Extract custom delimiter logic into separate methods

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,19 +1,28 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    
+
     parse_numbers(numbers).sum
   end
 
   private
 
   def parse_numbers(numbers)
-    if numbers.start_with?("//")
-      delimiter = numbers[2]
-      numbers_part = numbers.split("\n", 2)[1]
+    if has_custom_delimiter?(numbers)
+      delimiter, numbers_part = extract_custom_delimiter(numbers)
       numbers_part.split(delimiter).map(&:to_i)
     else
       numbers.split(/[,\n]/).map(&:to_i)
     end
+  end
+
+  def has_custom_delimiter?(numbers)
+    numbers.start_with?("//")
+  end
+
+  def extract_custom_delimiter(numbers)
+    delimiter = numbers[2]
+    numbers_part = numbers.split("\n", 2)[1]
+    [ delimiter, numbers_part ]
   end
 end
